67f38e435081fa1947166443481d19fdae97f7ec
Carson Rottinghaus <cqrottinghaus@gmail.com>
2021-12-05 16:15:47 -0600

    Initial Commit

---
+++ Butterfly Hunter/Assets/playerScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    private Vector3 _target;
+    public Camera Camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _target = Camera.ScreenToWorldPoint(Input.mousePosition);
+        _target.z = 0;
+        transform.position = Vector3.MoveTowards(transform.position, _target, 1);
+    }
+}
